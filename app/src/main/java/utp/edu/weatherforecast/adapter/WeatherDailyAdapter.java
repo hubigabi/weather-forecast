da84b27ab2e5839e2dc0ab90534e7784af7d3040
hubigabi <hubigabi19@gmail.com>
2021-05-31 03:12:24 +0200

    Changed layout for daily weather

--- app/src/main/java/utp/edu/weatherforecast/adapter/WeatherDailyAdapter.java
+++ app/src/main/java/utp/edu/weatherforecast/adapter/WeatherDailyAdapter.java
@@ -9,7 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import utp.edu.weatherforecast.R;
 import utp.edu.weatherforecast.entity.WeatherDaily;
@@ -28,21 +32,32 @@ public class WeatherDailyAdapter extends RecyclerView.Adapter<WeatherDailyAdapte
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.weather_daily_list_item, parent, false);
         WeatherViewHolder weatherViewHolder = new WeatherViewHolder(view);
         weatherViewHolder.weatherImageView = view.findViewById(R.id.weather_image_view);
-        weatherViewHolder.weatherTextView = view.findViewById(R.id.weather_text_view);
+        weatherViewHolder.headerTextView = view.findViewById(R.id.header_text_view);
+        weatherViewHolder.windTextView = view.findViewById(R.id.wind_text_view);
+        weatherViewHolder.humidityTextView = view.findViewById(R.id.humidity_text_view);
+        weatherViewHolder.cloudinessTextView = view.findViewById(R.id.cloudiness_text_view);
+        weatherViewHolder.pressureTextView = view.findViewById(R.id.pressure_text_view);
+        weatherViewHolder.feelsLikeTextView = view.findViewById(R.id.feels_like_text_view);
+        weatherViewHolder.nightTextView = view.findViewById(R.id.night_text_view);
         return weatherViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull WeatherDailyAdapter.WeatherViewHolder holder, int position) {
         WeatherDaily weatherDaily = weatherDailyList.get(position);
-        holder.weatherTextView.setText(weatherDaily.toString());
         try {
             int id = R.drawable.class.getField("w" + weatherDaily.getIconWeather()).getInt(null);
             holder.weatherImageView.setImageResource(id);
         } catch (IllegalAccessException | NoSuchFieldException e) {
             e.printStackTrace();
         }
-
+        holder.headerTextView.setText(getHeader(weatherDaily));
+        holder.windTextView.setText(String.format(Locale.getDefault(), weatherDaily.getWindSpeed() + "m/s"));
+        holder.humidityTextView.setText(String.format(Locale.getDefault(), "%d%%", weatherDaily.getHumidity()));
+        holder.cloudinessTextView.setText(String.format(Locale.getDefault(), "%d%%", weatherDaily.getClouds()));
+        holder.pressureTextView.setText(String.format(Locale.getDefault(), "%d hPa", weatherDaily.getPressure()));
+        holder.feelsLikeTextView.setText(String.format(Locale.getDefault(), "%d \u2103", weatherDaily.getDayFeelsLike().intValue()));
+        holder.nightTextView.setText(String.format(Locale.getDefault(), "%d \u2103", weatherDaily.getNightTemp().intValue()));
     }
 
     @Override
@@ -52,10 +67,28 @@ public class WeatherDailyAdapter extends RecyclerView.Adapter<WeatherDailyAdapte
 
     public static class WeatherViewHolder extends RecyclerView.ViewHolder {
         public ImageView weatherImageView;
-        public TextView weatherTextView;
+        public TextView headerTextView;
+        public TextView windTextView;
+        public TextView humidityTextView;
+        public TextView cloudinessTextView;
+        public TextView pressureTextView;
+        public TextView feelsLikeTextView;
+        public TextView nightTextView;
 
         public WeatherViewHolder(@NonNull View itemView) {
             super(itemView);
         }
     }
+
+    private String getHeader(WeatherDaily weatherDaily) {
+        StringBuilder sb = new StringBuilder();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm dd.MM", Locale.getDefault());
+        dateFormat.setTimeZone(TimeZone.getDefault());
+        Date date = new Date((long) (weatherDaily.getDt() * 1000L));
+        sb.append(dateFormat.format(date)).append(System.lineSeparator());
+        sb.append(weatherDaily.getDayTemp().intValue()).append(" \u2103");
+        return sb.toString();
+    }
+
 }
